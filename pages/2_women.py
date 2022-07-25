5617a6f6e903bfe72840e2340b6acc21ce1d22b3
avishai231 <avishai231@gmail.com>
2022-07-25 22:42:32 +0300

    minor changes, some data, some scripting, mostly adding things to women section

--- pages/2_women.py
+++ pages/2_women.py
@@ -1,37 +1,28 @@
 import streamlit as st
 import pandas as pd
-from scripts.column import column
+from scripts.column import col_institute, col_subject
 from pathlib import Path
 import plotly.graph_objects as go
 
 st.markdown("""
-        # Under construction
+        # Women Professors in STEM
+        ## Under construction
         """)
 
 path = f"../data/women_stem.csv"
 stem_data = pd.read_csv(Path(__file__).parent / path)
-#st.table(stem_data)
 
-institute = st.sidebar.radio("Select institute to show women", ("Ariel", "Ben Gurion", "Bar Ilan", "Hebrew U.", "Weizmann",
+# Plot by institute
+institute = st.selectbox("Choose an institute to show data:", ("Ariel", "Ben Gurion", "Bar Ilan", "Hebrew U.", "Weizmann", 
     "Technion", "Tel-Aviv"))
-translation = {"Ariel":"Ariel","Ben Gurion":"BGU","Bar Ilan":"BIU","Hebrew U.":"HUJI","Weizmann":"WIS",
-        "Technion":"Technion","Tel-Aviv":"TAU"}
-inst = translation[institute]
+fig_data_inst = st.radio("Show absolute or precetage data in figures", ("precentage", "absolute"))
 
-part = stem_data[stem_data["institute"] == inst]
-#st.table(part)
+col_institute(stem_data, institute, fig_data_inst)
 
+# Plot by institute
+subject = st.selectbox("Choose a subject to compare institutes", ("chemistry", "biology", "physics", "math", "cs"))
+fig_data_dept = st.radio("Show absolute or precetage data in figures", ("precentage ", "absolute "))
 
-# Eventually move to individual function
-column_fig = go.Figure(data=[
-    go.Bar(name="Women", x=part["department"], y=part["women"]/part["total"]*100),
-    go.Bar(name="Men", x=part["department"], y=part["men"]/part["total"]*100)
-    ],
-    layout_yaxis_range=[0,100],
-    layout_title="Precentage of Women in STEM by Institution")
-st.plotly_chart(column_fig, use_container_width=False,sharing="streamlit")
-st.caption(f"Precentage of women in different STEM departments at {inst}.")
-
-
+col_subject(stem_data, subject, fig_data_dept)
 
 st.markdown("""- data was collected at the end of 2020""")
--- todo.md
+++ todo.md
@@ -1,12 +1,15 @@
 # To Do:
 ## Web App
-1. add statistical analysis of physic
-2. analysis for chemistry sankey 
-3. add more colors to sankey plot to account for extra physics departments (Haifa, OpenU)
+1. add more colors to sankey plot to account for extra physics departments (Haifa, OpenU)
+2. add women by absolute number by subject and institute: radio button to switch between them
+    implement in external function
+3. edit column_plot script to have one plotting function for each case and it switches according
+    to relevant input parameters
 
 ## Data
-1. generate women in STEM figures
-2. make list of missing data columns for each institute (then add this to web app to-do)
-3. automatic script to generate flow matrix (input for sankey) from tabular data
-4. correlation between precentage of PIs trained and preference of graduates from the same institute
-5. add Haifa university for physics
+1. make list of missing data columns for each institute (then add this to web app to-do)
+2. automatic script to generate flow matrix (input for sankey) from tabular data
+3. correlation between precentage of PIs trained and preference of graduates from the same institute
+4. add Haifa and OpenU university for physics
+5. add Haifa and OpenU university for chemistry
+6. add Haifa and OpenU to women STEM
