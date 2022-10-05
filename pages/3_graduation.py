cc34e63c02890ab56f34bb458ed1861712a88a60
avishai231 <avishai231@gmail.com>
2022-10-05 21:40:58 +0300

    added option to choose scale linear/log and simplified xkcd figure generation in the graduation script

--- pages/3_graduation.py
+++ pages/3_graduation.py
@@ -12,22 +12,26 @@ subject_list = ["physics", "chemistry", "biology"]
 degree_list = ["BSc", "MSc", "PhD"]
 
 subject = st.radio("Select a research field to show data for", subject_list)
-all_degrees = ptg.plot_all_degrees_for_subject(subject, degree_list)
+scale1 = st.radio("Scale", ("linear", "log"),key=1)
+all_degrees = ptg.plot_all_degrees_for_subject(subject, degree_list, scale1)
 st.pyplot(all_degrees)
 
 degree = st.radio("Select degrees to show data for", degree_list)
-trend_plot = ptg.plot_by_gender(subject, degree)
+scale2 = st.radio("Scale", ("linear", "log"),key=2)
+trend_plot = ptg.plot_by_gender(subject, degree, scale2)
 st.pyplot(trend_plot)
 
 
 institute_list = ["Ariel", "OpenU", "WIS", "BGU", "Haifa", "BIU", "TAU", "Technion", "HUJI"] 
-institute = st.selectbox("Choose an institute", institute_list) 
-by_institution = ptg.plot_by_institute(institute, subject)
+institute = st.selectbox("Choose an institute", institute_list)
+scale3 = st.radio("Scale", ("linear", "log"),key=3)
+by_institution = ptg.plot_by_institute(institute, subject, scale3)
 st.pyplot(by_institution)
 
 old_subject_data = st.radio("Choose parent science field", ["physical", "biological", "engineering and architecture", \
         "math, cs and statistics"])
-test = ptg.plot_old_data_graduation_all(old_subject_data, degree_list)
+scale4 = st.radio("Scale", ("linear", "log"),key=4)
+test = ptg.plot_old_data_graduation_all(old_subject_data, degree_list, scale4)
 st.pyplot(test)
 
 st.markdown("""
--- scripts/plot_trend_graduate.py
+++ scripts/plot_trend_graduate.py
@@ -25,78 +25,81 @@ def get_institute(subject, degree, institute):
     students = data[institute].tolist()
     return students, year
 
-def plot_by_gender(subject, degree, save=False):
+def plot_by_gender(subject, degree, scale="linear", save=False):
     women, men, year = get_path(subject, degree)
+    plt.xkcd()
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=90)
+    plt.yscale(scale)
+    ax.plot(year, men,'o-',markersize=5,label="men")
+    ax.plot(year, women,'o-',markersize=5,label="women")
+    ax.set_xlabel("year"); ax.set_ylabel("number")
+    ax.set_title(f"Number of {degree} graduates in {subject}")
+    ax.legend()
+    ax.grid(True, lw=0.5, zorder=0)
+    plt.ion() 
+    if save == True:
+        savepath = f"../images/"
+        plt.savefig(Path(__file__).parent / f"{savepath}{degree}_{subject}_xkcd.png")
+    return fig
 
-    with plt.xkcd():
-        fig, ax = plt.subplots(figsize=(8, 6), dpi=90)
-        ax.plot(year, men,'o-',markersize=5,label="men")
-        ax.plot(year, women,'o-',markersize=5,label="women")
-        ax.set_xlabel("year"); ax.set_ylabel("number")
-        ax.set_title(f"Number of {degree} graduates in {subject}")
-        ax.legend()
-        ax.grid(True, lw=0.5, zorder=0)
-        plt.ion() 
-        if save == True:
-            savepath = f"../images/"
-            plt.savefig(Path(__file__).parent / f"{savepath}{degree}_{subject}_xkcd.png")
-        return fig
+def plot_all_degrees_for_subject(subject, degrees, scale="linear", save=False):
+    plt.xkcd()
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=90)
+    plt.yscale(scale)
+    for deg in degrees:
+        women, men ,year = get_path(subject, deg)
+        students = [i+j for i,j in zip(women,men)]
+        ax.plot(year, students, "o-", markersize=5, label=deg)
+    ax.set_xlabel("year"); ax.set_ylabel("number of graduates")
+    ax.set_title(f"Number of graduates in {subject} by degree")
+    ax.legend()
+    ax.grid(True, lw=0.5, zorder=0)
+    plt.ion()
+    if save == True:
+        savepath = f"../images/"
+        plt.savefig(Path(__file__).parent / f"{savepath}all_degrees_{subj}_xkcd.png")
+    return fig
 
-def plot_all_degrees_for_subject(subject, degrees, save=False):
-    with plt.xkcd():
-        fig, ax = plt.subplots(figsize=(8, 6), dpi=90)
-        for deg in degrees:
-            women, men ,year = get_path(subject, deg)
-            students = [i+j for i,j in zip(women,men)]
-            ax.plot(year, students, "o-", markersize=5, label=deg)
-        ax.set_xlabel("year"); ax.set_ylabel("number of graduates")
-        ax.set_title(f"Number of graduates in {subject} by degree")
-        ax.legend()
-        ax.grid(True, lw=0.5, zorder=0)
-        plt.ion()
-        if save == True:
-            savepath = f"../images/"
-            plt.savefig(Path(__file__).parent / f"{savepath}all_degrees_{subj}_xkcd.png")
-        return fig
+def plot_by_institute(institute, subject, scale="linear", save=False):
+    plt.xkcd()
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=90)
+    plt.yscale(scale)
+    for deg in degrees:
+        students, year = get_institute(subject, deg, institute)
+        ax.plot(year, students,'o-',markersize=5,label=deg)
+        ax.set_xlabel("year")
+        ax.set_ylabel("number of graduates")
+        ax.set_title(f"Number of {subject} graduates by degree at the {institute.capitalize()}")
+    ax.grid(True, lw=0.5, zorder=0)
+    plt.ion()
+    ax.legend()
+    if save == True:
+        savepath = f"../images/"
+        plt.savefig(f"bsc_{institute}_chemistry_xkcd.png")
+    return fig
 
-def plot_by_institute(institute, subject, save=False):
-    with plt.xkcd():
-        fig, ax = plt.subplots(figsize=(8, 6), dpi=90)
-        for deg in degrees:
-            students, year = get_institute(subject, deg, institute)
-            ax.plot(year, students,'o-',markersize=5,label=deg)
-            ax.set_xlabel("year")
-            ax.set_ylabel("number of graduates")
-            ax.set_title(f"Number of {subject} graduates by degree at the {institute.capitalize()}")
-        ax.grid(True, lw=0.5, zorder=0)
-        plt.ion()
-        ax.legend()
-        if save == True:
-            savepath = f"../images/"
-            plt.savefig(f"bsc_{institute}_chemistry_xkcd.png")
-        return fig
-
-def plot_old_data_graduation_all(subject, degrees, save=False):
+def plot_old_data_graduation_all(subject, degrees, scale="linear" ,save=False):
     filepath = f"../data/combined_data_old.csv"
     path = Path(__file__).parent / filepath
     data = pd.read_csv(path)
+    plt.xkcd()
+    fig, ax = plt.subplots(figsize=(8, 6), dpi=90)
+    plt.yscale(scale)
+    ax.set_title(f"Number of graduates in the {subject} sciences")
     if subject == "engineering and architecture": subject = "engineer"
     elif subject == "math, cs and statistics": subject = "math"
-    with plt.xkcd():
-        fig, ax = plt.subplots(figsize=(8, 6), dpi=90)
-        year = data["year"].tolist()  
-        for deg in degrees:
-            grad_number = data[f"{deg.lower()}_{subject}"] 
-            ax.plot(year, grad_number, "o-", markersize=5, label=deg)
-        ax.set_xlabel("year"); ax.set_ylabel("number of graduates")
-        ax.set_title(f"Number of graduates in the {subject} sciences")
-        ax.legend()
-        ax.grid(True, lw=0.5, zorder=0)
-        plt.ion()
-        if save == True:
-            savepath = f"../images/"
-            plt.savefig(Path(__file__).parent / f"{savepath}all_degrees_{subj}_xkcd.png")
-        return fig
+    year = data["year"].tolist()  
+    for deg in degrees:
+        grad_number = data[f"{deg.lower()}_{subject}"] 
+        ax.plot(year, grad_number, "o-", markersize=5, label=deg)
+    ax.set_xlabel("year"); ax.set_ylabel("number of graduates")
+    ax.legend()
+    ax.grid(True, lw=0.5, zorder=0)
+    plt.ion()
+    if save == True:
+        savepath = f"../images/"
+        plt.savefig(Path(__file__).parent / f"{savepath}all_degrees_{subj}_xkcd.png")
+    return fig
 
 if __name__ == "__main__":
     pass
