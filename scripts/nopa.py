dbbfe7d5e674aff90844e48c2511173ae2fb0b76
avishai231 <avishai231@gmail.com>
2022-10-19 21:13:49 +0300

    organized nopa script

--- scripts/nopa.py
+++ scripts/nopa.py
@@ -8,20 +8,22 @@ def nopa(filepath):
     h_i ::: all hired that did phd in i
     returns r_ii/h_i
     """
-    pand = pd.read_csv(filepath,delimiter='\t',index_col=0)
-    cols = pand.columns.tolist()
+    data = pd.read_csv(filepath,delimiter='\t',index_col=0)
+    cols = data.columns
     nopa_list = []
+    nopa_i = []
     for i,j in enumerate(cols):
-        row_i = pand.iloc[i]
+        row_i = data.iloc[i]
         sum_i = sum(row_i)
         inst = cols[i]
         if sum_i != 0:
             nopa = row_i[i]/sum_i
         else:
             nopa = 0
+        nopa_i.append(round(nopa*100,2))
         nopa_list.append((inst,nopa))
-    #nopa_df = pd.DataFrame() 
-    return nopa_list
+    nopa_df = pd.DataFrame(index=cols, data={"NOPA (%)":nopa_i})
+    return nopa_df
 
 if __name__ == "__main__":
     from pathlib import Path
@@ -29,5 +31,5 @@ if __name__ == "__main__":
     path = f"../data/physics_data.txt"
     nopa = nopa(Path(__file__).parent / path)
     #print(nopa)
-    print(np.array(nopa))
+    #print(np.array(nopa))
     #print(pd.DataFrame(nopa))
